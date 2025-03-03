6b768545555847fd7de00728bf8c1cdf42f1abb2
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-03-03 18:31:45 +0100

    feat: add form login and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './authenticated_section/home/home.component';
+import { HomeUnauthenticatedComponent } from './unauthenticated_section/home-unauthenticated/home-unauthenticated.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
+
+  { path: "", component: HomeUnauthenticatedComponent },
+  { path: "home", component: HomeComponent },
   { path: "**", redirectTo: "", pathMatch: "full" }
 ];
 
