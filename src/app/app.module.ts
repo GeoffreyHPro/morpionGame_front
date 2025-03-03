caec268ad18c6e627168e11f7724d805e558b0ad
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-03-03 10:26:26 +0100

    feat: add base project

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RxStompService } from '@stomp/ng2-stompjs';
+import { HomeComponent } from './unauthenticated_section/home/home.component';
+import { ChatComponent } from './unauthenticated_section/home/chat/chat.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    RxStompService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
