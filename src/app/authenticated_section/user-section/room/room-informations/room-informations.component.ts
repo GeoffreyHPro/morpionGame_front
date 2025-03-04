11562fffb9f2a480ddccac585117db493740ca2b
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-03-04 19:07:56 +0100

    feat: leave room, remove you from roomManager and redirect to list of rooms view

--- src/app/authenticated_section/user-section/room/room-informations/room-informations.component.ts
+++ src/app/authenticated_section/user-section/room/room-informations/room-informations.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { WebsocketService } from '../../../../service/websocket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-informations',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './room-informations.component.css'
 })
 export class RoomInformationsComponent {
+  @Input() roomId!: string | null;
+  @Input() username!: string;
 
+  constructor(
+    private webSocket: WebsocketService,
+    private router: Router
+  ) { }
+
+  handleLeaveButton() {
+    console.log(this.roomId)
+    console.log(this.username)
+    this.webSocket.sendMessageLeaveRoom(this.roomId!, this.username);
+    this.router.navigateByUrl("home");
+  }
 }
